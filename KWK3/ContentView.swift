c611bf4eb8516a7331396bc7ab1e5bba8a8ca119
Stella <stellakoenings@gmail.com>
2023-06-26 12:29:38 -0500

    Initial Commit

---
+++ KWK3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  KWK3
+//
+//  Created by Stella Koenings on 6/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KWK3/KWK3App.swift
@@ -0,0 +1,17 @@
+//
+//  KWK3App.swift
+//  KWK3
+//
+//  Created by Stella Koenings on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct KWK3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
